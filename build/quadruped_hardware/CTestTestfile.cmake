f363c40fb270fd6494cbd099ac3f7bab7daa4b26
WestJeremy <116618595+WestJeremy@users.noreply.github.com>
2024-12-18 20:04:49 +0000

    clean up uncessesary files

--- build/quadruped_hardware/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ws/src/quadruped_hardware
-# Build directory: /home/ws/build/quadruped_hardware
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
