1022575fae826748fa8adc3cd889528fe051507d
westjeremy <westjeremy6@gmail.com>
2025-04-23 22:51:09 +0000

    imu serial read is bad

--- src/cpp_pubsub/src/readUSB.cpp
+++ src/cpp_pubsub/src/readUSB.cpp
@@ -40,14 +40,19 @@ class MinimalPublisher : public rclcpp::Node
 {
 public:
   MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
+  : Node("imu_publisher"), count_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+
+    // two publishers 
+    publisher_ = this->create_publisher<std_msgs::msg::String>("pitch", 10);
+
+
     auto timer_callback =
       [this]() -> void {
         auto message = std_msgs::msg::String();
         message.data = "Hello, world! " + std::to_string(this->count_++);
         RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        
         this->publisher_->publish(message);
       };
     timer_ = this->create_wall_timer(500ms, timer_callback);
