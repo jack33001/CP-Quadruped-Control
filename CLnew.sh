2057c16311880edc9abc20c23ce549227430ed49
westjeremy <westjeremy6@gmail.com>
2025-05-08 22:38:03 +0000

    imu works with ros2 control

--- CLnew.sh
+++ CLnew.sh
@@ -1,5 +1,5 @@
 # sudo chmod a+rw /dev/ttyACM0
-# sudo chmod a+rw /dev/ttyUSB0
+sudo chmod a+rw /dev/ttyUSB0
 source /opt/ros/jazzy/setup.bash
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 source install/setup.bash
